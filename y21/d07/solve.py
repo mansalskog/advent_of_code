5ca4dc1637c301840fc1ecaaad8cc22bad41eafb
Måns Alskog <manal270@student.liu.se>
2021-12-08 08:00:09 +0100

    Day 8

--- y21/d07/solve.py
+++ y21/d07/solve.py
@@ -1,20 +1,15 @@
 import sys
 
+def tri(n):
+    return n * (n + 1) // 2
+
 with open(sys.argv[1]) as f:
     xs = [int(s) for s in f.readline()[:-1].split(',')]
 
-def tri(n):
-    # return sum(range(n+1))
-    return n * (n + 1) // 2
+print(min(
+    sum(abs(x - x0) for x in xs)
+    for x0 in range(min(xs), max(xs))))
 
-min_c = float('inf')
-min_x0 = None
-for x0 in range(min(xs), max(xs)):
-    c = 0
-    for x in xs:
-        # c += abs(x - x0)
-        c += tri(abs(x - x0))
-    if c < min_c:
-        min_c = c
-        min_x0 = x0
-print(min_c, 'at', min_x0)
+print(min(
+    sum(tri(abs(x - x0)) for x in xs)
+    for x0 in range(min(xs), max(xs))))
