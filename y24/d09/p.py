49c4c602bd6cc89ff05be824275ab021ad0f60df
Måns Alskog <mans.alskog@nodeus.se>
2024-12-11 09:58:41 +0100

    Another uncool (and slow) python solution for day 9

--- y24/d09/p.py
+++ y24/d09/p.py
@@ -19,13 +19,34 @@ def compress(file):
         # print(file)
         to_move -= 1
 
+def compress_whole(file):
+    to_move_fnum = max(fnum for fnum in file)
+    while to_move_fnum > 0:
+        print(to_move_fnum)
+        file_start = file.index(to_move_fnum)
+        file_end = last_index(file, to_move_fnum)
+        file_len = file_end - file_start + 1
+        for dest in range(0, file_start):
+            if all(fnum == -1 for fnum in file[dest:(dest+file_len)]):
+                # print("moving file", to_move_fnum, "from", file_start, "to", dest)
+                file[file_start:file_end+1] = [-1] * file_len
+                file[dest:(dest+file_len)] = [to_move_fnum] * file_len
+                break
+        to_move_fnum -= 1
+        # print(file)
+
+def last_index(l, e):
+    return len(l) - 1 - l[::-1].index(e)
+
 def checksum(file):
     return sum(idx * pos if idx != -1 else 0 for (pos,idx) in enumerate(file))
 
 dm_text = input()
 dm = list(map(int, dm_text))
-# print(dm)
+# file = makefile(dm)
+# compress(file)
+# print(checksum(file))
+
 file = makefile(dm)
-# print(file)
-compress(file)
+compress_whole(file)
 print(checksum(file))
